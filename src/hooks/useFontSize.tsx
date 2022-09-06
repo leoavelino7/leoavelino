5b9f58f42f9e0bd4e60cc51b06a782b4c99a5cdd
Leonardo Avelino <leonardo.avelino16@hotmail.com>
2022-09-06 20:24:24 -0300

    fix(custom-fonts): font size locale by locale (#4)
    Co-authored-by: Leonardo Avelino <>

--- src/hooks/useFontSize.tsx
+++ src/hooks/useFontSize.tsx
@@ -1,40 +1,42 @@
 import { useEffect, useRef } from "react";
+import { useTranslation } from "react-i18next";
 import { useLocalStorage } from "./useLocalStorage";
 
 export type FontSizeClassName = "text-xs" | "text-sm" | "text-md" | "text-lg" | "text-xl";
 
 export type FontSize = { children: JSX.Element | string; value: FontSizeClassName };
 
-export const fontSizeList: FontSize[] = [
-  {
-    children: "Extra small",
-    value: "text-xs"
-  },
-  {
-    children: "Small",
-    value: "text-sm"
-  },
-  {
-    children: "Default",
-    value: "text-md"
-  },
-  {
-    children: "Large",
-    value: "text-lg"
-  },
-  {
-    children: "Extra Large",
-    value: "text-xl"
-  }
-];
-
-const fontSizeValue: FontSizeClassName[] = fontSizeList.map((theme) => theme.value);
-
-const isSupportedFontSize = (fontSize: string) => fontSizeValue.includes(fontSize as FontSizeClassName);
-
-export const useFontSize = (): [string, (fontSize: string) => void] => {
+export const useFontSize = (): [string, (fontSize: string) => void, FontSize[]] => {
+  const { t } = useTranslation("common");
   const [fontSize, setFontSize] = useLocalStorage("@fontSize", "text-md");
   const lastFontSizeRef = useRef(fontSize);
+
+  const fontSizeList: FontSize[] = [
+    {
+      children: t("header_font_size_extra_small"),
+      value: "text-xs"
+    },
+    {
+      children: t("header_font_size_small"),
+      value: "text-sm"
+    },
+    {
+      children: t("header_font_size_default"),
+      value: "text-md"
+    },
+    {
+      children: t("header_font_size_large"),
+      value: "text-lg"
+    },
+    {
+      children: t("header_font_size_extra_large"),
+      value: "text-xl"
+    }
+  ];
+
+  const fontSizeValue: FontSizeClassName[] = fontSizeList.map((theme) => theme.value);
+
+  const isSupportedFontSize = (fontSize: string) => fontSizeValue.includes(fontSize as FontSizeClassName);
 
   const changeFontSize = (selectedFontSize: string) => {
     if (isSupportedFontSize(selectedFontSize) && selectedFontSize !== fontSize) {
@@ -51,5 +53,5 @@ export const useFontSize = (): [string, (fontSize: string) => void] => {
     }
   }, [fontSize]);
 
-  return [fontSize, changeFontSize];
+  return [fontSize, changeFontSize, fontSizeList];
 };
